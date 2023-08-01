0252c909466beee489c10d5b8135b53b740c896d
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-01 12:53:05 -0300

    criando auth page e funcionalidades

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../components/auth_form.dart';
+import '../models/auth_form_data.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+
+    print('AuthPage...');
+    print(formData.email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(children: [
+        Stack(children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5)
+              ),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+        ]),
+      ]),
+    );
+  }
+}
