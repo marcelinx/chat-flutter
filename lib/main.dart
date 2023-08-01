192af2c99235c12f5fc6e45e29ee582dbc2d9ee5
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-01 11:53:09 -0300

    setting files

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat/pages/loading_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoadingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
