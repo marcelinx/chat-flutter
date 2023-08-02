6f0934815e99ed73f608f5b4bd9b3e530be39618
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-02 11:25:58 -0300

    fixing bugs e adicionando nova funcionalidade

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,13 +1,25 @@
+import 'package:chat/core/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({ Key? key }) : super(key: key);
+  const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Chat Page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Chat page'),
+            TextButton(
+              onPressed: () {
+                AuthMockService().logout();
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
       ),
     );
   }
